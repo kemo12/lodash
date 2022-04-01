469c06b55b435633463baba27871a9094f28abe6
kemo12 <dodkamel919@gmail.com>
2022-04-01 17:10:54 +0300

    level_3/object_Assign

---
+++ level_3/object_assign.js
@@ -0,0 +1,15 @@
+function Foo() {
+      this.a = 1;
+    }
+     
+function Bar() {
+      this.c = 3;
+    }
+Foo.prototype.b = 2;
+Bar.prototype.d = 4;
+
+function assign(object,...source){
+return Object.assign(...source);
+}
+
+console.log(assign({'a':0},new Foo,new Bar))
--- level_4/array_flatten_deep.js
+++ level_4/array_flatten_deep.js
@@ -1,7 +1,7 @@
 function flattenDeep(array){
-    
+
     function getArrayDepth(arr) {
-        return Array.isArray(arr) ? 1 + Math.max(0, ...arr.map(getArrayDepth)) :0;
+        return Array.isArray(arr) ? 1 + Math.max(0, ...arr.map(getArrayDepth)):0;
     }
 
       if(getArrayDepth(array)==1) return array;
