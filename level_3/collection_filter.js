afc08f72ebdeceecc69567131081abb7b900543d
kemo12 <dodkamel919@gmail.com>
2022-04-02 00:20:59 +0300

    level_3/ collection_filter

--- level_3/collection_filter.js
+++ level_3/collection_filter.js
@@ -1,1 +1,13 @@
-console.log("first")
+var users = [
+    { 'user': 'barney', 'age': 36, 'active': true },
+    { 'user': 'fred',   'age': 40, 'active': false },
+  ];
+
+function filter(collection,predicate){
+    let filteredArray=[];
+    for(let i=0;i<collection.length;i++){
+        if (predicate(collection[i]))filteredArray.push(collection[i]);
+    }
+    return filteredArray;
+}
+console.log(filter(users, function(o) { return !o.active; }))
