cf725b652f1e0464f8e1fd72103514ef27792beb
kemo12 <dodkamel919@gmail.com>
2022-04-02 01:36:15 +0300

    level_1/array_difference update

--- level_1/array_difference.js
+++ level_1/array_difference.js
@@ -1,6 +1,6 @@
 function difference(...arrays){
-let firstArr=arrays.shift().filter((e)=>!arrays.flat(1).includes(e))
-console.log(firstArr)
+return arrays.shift().filter((e)=>!arrays.flat(1).includes(e));
+
 }
 
 console.log(difference([2, 1], [1, 3]));
