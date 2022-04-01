352de743d9ca4401371e6471445d9db285a86e41
kemo12 <dodkamel919@gmail.com>
2022-04-01 16:41:00 +0300

    level_4/ array_flatten_deep

---
+++ level_4/array_flatten_deep.js
@@ -0,0 +1,10 @@
+function flattenDeep(array){
+    
+    function getArrayDepth(arr) {
+        return Array.isArray(arr) ? 1 + Math.max(0, ...arr.map(getArrayDepth)) :0;
+    }
+
+      if(getArrayDepth(array)==1) return array;
+      return flattenDeep(array.flat());      
+}
+console.log(flattenDeep([1, [2, [3, [4]], 5]]))
